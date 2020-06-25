6148477e834c36f640722abfb72a52e64a7ef8bf
wilder <francois.yhuel@hotmail.fr>
2020-06-25 09:11:01 +0200

    initialisation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import storm from "../img/storm.jpeg";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>La mer est bleue</h1>
+    </div>
+  );
+};
+
+export default Navbar;
