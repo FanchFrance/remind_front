dc72cd03ba6532cde24ad76f6a218dee724669e4
wilder <francois.yhuel@hotmail.fr>
2020-06-26 08:31:38 +0200

    symptoms link to nextpill

--- src/components/Symptoms.jsx
+++ src/components/Symptoms.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 const symptom = () => {
   return (
@@ -7,44 +8,60 @@ const symptom = () => {
       <h1 className="symptoms_title">Mes symptômes</h1>
       <div className="symptoms">
         <div>
-          <button className="symptom">
-            <h3>Je tremble</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>Je tremble</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>Je suis bloqué</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>Je suis bloqué</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>perte de l'équilibre</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>perte de l'équilibre</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>crampes</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>crampes</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>je respire mal</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>je respire mal</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>hallucinations visuelles</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>hallucinations visuelles</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>dépression</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>dépression</h3>
+            </button>
+          </Link>
         </div>
         <div>
-          <button className="symptom">
-            <h3>trouble du sommeil</h3>
-          </button>
+          <Link to="./nextpill">
+            <button className="symptom">
+              <h3>trouble du sommeil</h3>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
