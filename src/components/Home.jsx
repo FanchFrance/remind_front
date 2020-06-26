eb5402f6be50fd69d9d553c63da0142217aec40b
bz699 <zeebha@gmail.com>
2020-06-26 03:31:22 +0200

    add login page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
-import {Form, Button} from 'react-bootstrap';
+
+import './styles/home.css';
 
 const Home = () => {
 
   return(
     <>
-      <header>REMIND</header>
-      <div className="Login"></div>
-      <div className="Register"></div>
+    
+    <div className="contain">
+      <div className="headr"><h1>REMIND</h1></div>
+      <div className="loging">
+      <h3>Log in</h3>
+        <form>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btton">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+      </div>
+      
+      <div className="register">
+        <button type="submit" className="btton">Créer un Compte</button>
+      </div>
+      
+    </div>
     </>
   )
 }
